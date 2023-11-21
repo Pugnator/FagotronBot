c65806c5888fa4c618cee801497280e94ab3cead
Pugnator <ookami@mail.ru>
2023-11-22 00:03:33 +0100

    Working on the project setup

--- fagotron.cc
+++ fagotron.cc
@@ -2,7 +2,7 @@
 
 #include <tgbot/tgbot.h>
 #include "log.hpp"
-#include "botcommander.hpp"
+#include "fagotron.hpp"
 
 std::atomic<bool> sigintReceived(false);
 
@@ -14,42 +14,16 @@ void handleSignal(int s)
 
 int main()
 {
-
-  TgBot::Bot bot(getenv("TOKEN"));
+  TgBot::Bot bot(getenv("FAGOTRON_TOKEN"));
   Log::get().configure(TraceType::file).set_level(TraceSeverity::debug);
-  LOG_INFO("Bot {} started\n", bot.getApi().getMe()->username);
-
-  Bot::BotCommander::Ptr commander = std::make_unique<Bot::BotCommander>(bot);
+  LOG_INFO("Bot {} started\n", bot.getApi().getMe()->username);  
 
   // Set commands
-  std::vector<TgBot::BotCommand::Ptr> commands;
-  TgBot::BotCommand::Ptr cmdArray = std::make_shared<TgBot::BotCommand>();
-  cmdArray->command = "search";
-  cmdArray->description = "Search for a word, kanji or example in the dictionary.";
+  std::vector<TgBot::BotCommand::Ptr> commands;  
+  cmdArray->command = "play";
+  cmdArray->description = U"Кто сегодня герой?";
   commands.push_back(cmdArray);
-  cmdArray = std::make_shared<TgBot::BotCommand>();
-  cmdArray->command = "quiz";
-  cmdArray->description = "Start a quiz. You can choose between diffeeent types of questions.";
-  commands.push_back(cmdArray);
-  cmdArray = std::make_shared<TgBot::BotCommand>();
-  cmdArray->command = "difficulty";
-  cmdArray->description = "Set the difficulty level for the quiz.";
-  commands.push_back(cmdArray);
-  // cmdArray = std::make_shared<TgBot::BotCommand>();
-  // cmdArray->command = "explain";
-  // cmdArray->description = "Explain a word, kanji or example.";
-  // commands.push_back(cmdArray);
-  bot.getApi().setMyCommands(commands);
-
-  // Set event handlers
-  bot.getEvents().onUnknownCommand([&commander](TgBot::Message::Ptr message)
-                                   { commander->parseCommand(message); });
-
-  bot.getEvents().onNonCommandMessage([&commander](TgBot::Message::Ptr message)
-                                      { commander->parseUserInputAsync(message); });
-
-  bot.getEvents().onCallbackQuery([&commander](TgBot::CallbackQuery::Ptr query)
-                                  { commander->parseCallback(query); });
+  bot.getApi().setMyCommands(commands); 
 
   signal(SIGINT, handleSignal);
 
